9fb0d3725ad22cfe8c7f4e7c8aa85d7726bad002
bryanmarichal24 <68909747+bryanmarichal24@users.noreply.github.com>
2020-09-19 18:58:51 -0300

    19_9_20-Menu y login de usuario completos

--- paginas/js/login.js
+++ paginas/js/login.js
@@ -1,6 +1,20 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+function saveUser(usuario){
+    localStorage.clear();
+    localStorage.setItem("user",usuario);
+    //window.location.href="paginas/inicio.html";
+    console.log("hola");
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+    localStorage.clear();
+    localStorage.removeItem("user");
+    
 });
+
+/*document.getElementById("botonIniciar").addEventListener("click", function(e){
+    saveUser();
+})*/
